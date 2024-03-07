fa93d2c92ce793e75fd2d1a8f078bb0b81c6acb0
JP Hwang <me@jphwang.com>
2024-03-07 10:53:12 +0100

    Update script

---
+++ test_mem_usage.py
@@ -0,0 +1,77 @@
+import weaviate.classes.config as wc
+import weaviate.classes.data as wd
+import weaviate.classes.query as wq
+from weaviate.collections import Collection
+from tqdm import tqdm
+from weaviate.util import generate_uuid5
+import weaviate
+import h5py
+from pathlib import Path
+from utils import measure_mem_usage, parse_results_text
+
+
+N_MAX_IMPORT = 1000000
+COLL_NAME = "DBPedia"
+
+
+def reconfigure_pq(tgt_collection: Collection):
+    tgt_collection.config.update(
+        vector_index_config=wc.Reconfigure.VectorIndex.hnsw(
+            quantizer=wc.Reconfigure.VectorIndex.Quantizer.pq()
+        )
+    )
+
+
+def do_nothing(*args, **kwargs):
+    pass
+
+
+# Load data
+sift_file = Path("data/dbpedia-openai-1000k-angular.hdf5")
+f = h5py.File(sift_file)
+train_vectors = f["train"]
+
+
+# Connect to Weaviate
+client = weaviate.connect_to_local()
+
+for output_prefix, reconfigure_function, vector_index_config in [
+    ("hnsw_uncompressed", do_nothing, wc.Configure.VectorIndex.hnsw()),
+    ("hnsw_pq", reconfigure_pq, wc.Configure.VectorIndex.hnsw()),
+    ("hnsw_bq", do_nothing, wc.Configure.VectorIndex.hnsw(quantizer=wc.Configure.VectorIndex.Quantizer.bq())),
+    ("flat_bq", do_nothing, wc.Configure.VectorIndex.flat(quantizer=wc.Configure.VectorIndex.Quantizer.bq())),
+    ("flat_uncompressed", do_nothing, wc.Configure.VectorIndex.flat()),
+]:
+
+    client.collections.delete(COLL_NAME)
+
+    # Create collection
+    collection = client.collections.create(
+        name=COLL_NAME,
+        properties=[
+            wc.Property(name="name", data_type=wc.DataType.TEXT),
+        ],
+        vector_index_config=vector_index_config
+    )
+
+    # Add objects
+    with collection.batch.dynamic() as batch:
+        for i, vector in enumerate(tqdm(train_vectors[:N_MAX_IMPORT])):
+            real_i = i + 1  # 1-based index
+
+            padding = "0" * (7 - len(str(real_i)))
+            properties = {"name": f"DBPedia_{padding}{real_i}"}
+            batch.add_object(
+                properties=properties,
+                uuid=generate_uuid5(real_i),
+                vector=list(vector)
+            )
+
+            if real_i == 100000:
+                reconfigure_function(collection)
+
+            if real_i % 100000 == 0:
+                measure_mem_usage(f"{output_prefix}_{real_i}")
+
+
+parse_results_text()
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import time
 import subprocess
+from pathlib import Path
+import re
+import json
 
 
 def measure_mem_usage(suffix: str):
@@ -13,3 +16,36 @@ def measure_mem_usage(suffix: str):
 
     print("Memory usage measured.")
     print(f"Output saved to mem_usage_{suffix}.txt")
+
+
+def parse_results_text():
+    output_dir = Path("output")
+    output_files = output_dir.glob("*.txt")
+
+    benchmark_outputs = list()
+    for f in output_files:
+        print(f"Processing {f}...")
+        txt = f.read_text()
+        # Find the memory footprint in "of 624.83MB total" type pattern
+        match = re.search(r"of (\d+\.\d+)MB total", txt)
+        if match:
+            mem_usage = float(match.group(1))
+        else:
+            print(f"Could not find memory usage in {f}")
+            mem_usage = None
+
+        fname_split = f.name.split("_")
+        result = {
+            "index_type": fname_split[2],
+            "compression": fname_split[3],
+            "object_count": int(fname_split[4].split(".")[0]),
+            "memory_usage": mem_usage
+        }
+        benchmark_outputs.append(result)
+
+        print("Done.")
+
+    with open("benchmark_outputs.json", "w") as f:
+        json.dump(benchmark_outputs, f, indent=2)
+
+    return True
